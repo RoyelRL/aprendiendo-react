114ecdfb0d93e17fe742f90538defb2a11c38d4b
RoyelRL <franko.ramos@iestpmonfortianos.edu.pe>
2023-06-30 22:07:11 -0500

    creando mi primer commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import PresentationCard from './presentationcard'
+import './App.css'
+
+
+function App() {
+  /*const [count, setCount] = useState(0)
+*/
+
+  return (
+    <div className="App">
+    {/*<p>hola mundo{10 + 20}</p>*/}
+      <PresentationCard/>
+     
+    </div>
+  )
+}
+
+export default App
---
+++ src/PresentationCard.jsx
@@ -0,0 +1,8 @@
+function PresentationCard(){
+    return(
+        <p>hola desde mi primer componente</p>
+
+    );
+
+}
+export default PresentationCard;
